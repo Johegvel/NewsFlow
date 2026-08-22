060bbb7a23ac131f134ad82a89e77da95913b625
Xavih830 <xhcg2003@gmail.com>
2026-08-22 13:19:04 -0500

    fix: set require_master_key=false in production and enforce RAILS_ENV in puma.rb

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
+  # Disable requirement for master.key when SECRET_KEY_BASE is passed
+  config.require_master_key = false
+
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
